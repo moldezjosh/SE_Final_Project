9b1fe4e78fad345788c03949f7c46fcb2434f22e
Joshua Mark Moldez <j.markmoldez@gmail.com>
2017-11-27 10:05:01 +0800

    modified manage user

--- manageuser.php
+++ manageuser.php
@@ -1,9 +1,15 @@
+<?php
+	session_start();
+	//require_once 'include/delete.php';
+?>
 <!DOCTYPE html>
 <head>
 	<title>WebDTS</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
   <script type="text/javascript" src="js/scripts.js"></script>
-
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 	<header>
@@ -25,11 +31,11 @@
       </form>
 
       <div class="dropdown">
-        <button onclick="dropFunc()" class="dropbtn">User</button>
+        <button onclick="dropFunc()" class="dropbtn"><?php echo $_SESSION['username']; ?></button>
         <div id="myDropdown" class="dropdown-content">
 					<a href="index.php">Dashboard</a>
           <a href="usersetting.php">My Menus</a>
-          <a href="login.php">Logout</a>
+          <a href="include/logout.php">Logout</a>
         </div>
       </div>
     </div>
@@ -48,36 +54,62 @@
           </td>
           <td class="am-display">
             <h3 class="nav-header">manage user</h3>
-            <div class="menu-display" style="height: 450px">
-								<table class="user-list">
-									<tr class="user-header">
-										<th>ID</th>
-										<th>Fullname</th>
-										<th>Email</th>
-										<th>Office</th>
-										<th>Position</th>
-										<th>Is Admin?</th>
-										<th>Action</th>
-									</tr>
-									<tr class="user-data">
-										<td>maryelisse</td>
-										<td class="fullname-user">mary elisse gonzales</td>
-										<td>maryelisse@gmail.com</td>
-										<td>BUDGET</td>
-										<td>Edit Position Here</td>
-										<td>user</td>
-										<td><a href=""><img src="img/delete-ico.png" title="Delete"></a></td>
-									</tr>
-								</table>
+            <div class="menu-display" style="height: 100%">
+
+
+							<?php
+							// Include config file
+						require_once 'include/config.php';
+
+							// Attempt select query execution
+							$sql = "SELECT * FROM users";
+							if($result = mysqli_query($link, $sql)){
+									if(mysqli_num_rows($result) > 0) {
+										echo "<table class='user-list'>";
+												echo "<tr class='user-header'>";
+														echo "<th>Username</th>";
+														echo "<th>Fullname</th>";
+														echo "<th>Email</th>";
+														echo "<th>Office</th>";
+														echo "<th>Position</th>";
+														echo "<th>Is Admin?</th>";
+														echo "<th>Action</th>";
+												echo "</tr>";
+										while($row = mysqli_fetch_array($result)){
+												echo "<tr class='user-data'>";
+														echo "<td>". $row['username'] ."</td>";
+														echo "<td class='fullname-user'>". $row['name'] ."</td>";
+														echo "<td>". $row['email'] ."</td>";
+														echo "<td>". $row['office'] ."</td>";
+														echo "<td> Edit Position Here</td>";
+														echo "<td>". $row['userType'] ."</td>"; ?>
+														<td><a href="#del<?php echo $row['id']; ?>" data-toggle="modal"><img src="img/delete-ico.png" class="btn btn-info btn-lg" title="Delete"></a></td>
+														<?php
+														include('action.php');
+												echo "</tr>";
+										}
+										echo "</table>";
+										// Free result set
+										mysqli_free_result($result);
+								} else {
+										echo "<p class='lead'><em>No account(s) were found.</em></p>";
+									}
+								} else{
+										echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+								}
+
+						// Close connection
+						mysqli_close($link);
+						?>
             </div>
           </td>
         </tr>
       </table>
     </div>
-
   </div>
       <footer class="footer">
           <center><p class="copyright">COPYRIGHT © 2017. MINDANAO DEVELOPMENT AUTHORITY, REPUBLIC OF THE PHILIPPINES. ALL RIGHTS RESERVED.</p></center>
       </footer>
+
   </body>
 </html>
