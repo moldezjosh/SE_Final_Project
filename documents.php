981f5fe45ec114a3181dac11e6bc527783454a1b
Joshua Mark Moldez <j.markmoldez@gmail.com>
2017-11-27 09:58:36 +0800

    renamed assigneddocu to documents

--- documents.php
+++ documents.php
@@ -27,7 +27,7 @@
       <div class="dropdown">
         <button onclick="dropFunc()" class="dropbtn">User</button>
         <div id="myDropdown" class="dropdown-content">
-					<a href="assigneddocu.php">My Menus</a>
+					<a href="documents.php">My Menus</a>
           <a href="usersetting-user.php">Settings</a>
           <a href="login.php">Logout</a>
         </div>
@@ -42,43 +42,33 @@
           		<img src="img/minda-logo.png" alt="user-profile-pic">
           </td>
           <td class="am-display" rowspan="2" valign="top">
-            <h3 class="nav-header">all documents assigned to you</h3>
+            <h3 class="nav-header">all documents</h3>
               <div class="menu-display" style="height: 350px">
-								<div class="search-document">
-									<form action="" method="post">
-									<div class="search-div">
-											<input type="text" name="search-name" class="search-name" placeholder="Search"/>
-											<input type="submit" name="btnSearch" class="btnSearch" value="Search" />
-									</div>
-								</form>
 
 								<table class="user-list">
 									<tr class="user-header">
-										<th>Barcode</th>
+										<th>Code</th>
+										<th>Type</th>
 										<th>Sender</th>
-										<th>Document Type</th>
-										<th>Note</th>
-										<th>Date  </th>
-										<th>Action</th>
+										<th>Recipient</th>
+										<th>Date</th>
 										<th>Status</th>
+										<th>Action</th>
 									</tr>
 									<tr class="user-data">
-										<td>702649</td>
-										<td class="fullname-user">human resource management</td>
-										<td>Communication Letters</td>
-										<td>lorem ipsum dolor</td>
-										<td>Oct 29</td>
+										<td>2017-DTS-IN01</td>
+										<td>Request</td>
+										<td>From: Joshua Mark Moldez</td>
+										<td>Mary Elisse Gonzales</td>
+										<td>11/26/2017- 10:13 PM</td>
+										<td>Pending</td>
 										<td class="action-icons">
 												<a href="viewdocu.php"><img src="img/view-icon.png" title="View"></a>
-												<a href="#"><img src="img/reply-ico.png" title="Reply"></a>
-												<a href="#"><img src="img/finish-icon.png" title="Finish"></a>
 												<a href="#"><img src="img/delete-ico.png" title="Delete"></a>
 												</td>
-										<td><div class="status"><p>Pending</p></div></td>
-
 									</tr>
 								</table>
-							</div>
+
               </div>
             </td>
           </tr>
@@ -86,9 +76,8 @@
 						<div class="user-menus">
 						<h3 class="nav-header">menu</h3>
 						<ul>
-							<li><a href="createdocu.php"><img src="img/dashboard-icon.png" alt="dashboard-icon"><p>create document</p></a></li>
-							<li><a href="assigneddocu.php"><img src="img/create-user-icon.png" alt="crate-user-icon"><p>assigned document</p></a></li>
-							<li><a href="office.php"><img src="img/manage-user-icon.png" alt="manage-user-icon"><p>in your office</p></a></li>
+							<li><a href="adddocument.php"><img src="img/dashboard-icon.png" alt="dashboard-icon"><p>add document</p></a></li>
+							<li><a href="documents.php"><img src="img/create-user-icon.png" alt="crate-user-icon"><p>documents</p></a></li>
 							<li><a href="reports.php"><img src="img/user-setting-icon.png" alt="user-setting-icon"><p>reports</p></a></li>
 						</ul>
 					</div>
