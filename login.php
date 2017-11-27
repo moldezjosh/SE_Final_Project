2061ee88a0a27f8136912bedfea0b3d75e27c9cb
Joshua Mark Moldez <j.markmoldez@gmail.com>
2017-11-27 10:03:30 +0800

    modified login, deleted loginerr file

--- login.php
+++ login.php
@@ -1,3 +1,87 @@
+<?php
+	require_once 'include/config.php';
+	// Define variables and initialize with empty values
+	$username = $password = "";
+	$username_err = $password_err = "";
+
+	// Processing form data when form is submitted
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	    // Check if username is empty
+	    if(empty(trim($_POST["username"]))){
+	        $username_err = 'Please enter your username.';
+	    } else{
+	        $username = trim($_POST["username"]);
+	    }
+
+	    // Check if password is empty
+	    if(empty(trim($_POST['password']))){
+	        $password_err = 'Please enter your password.';
+	    } else{
+	        $password = trim($_POST['password']);
+	    }
+
+	    // Validate credentials
+	    if(empty($username_err) && empty($password_err)){
+	        // Prepare a select statement
+	        $sql = "SELECT username, password, userType FROM users WHERE username = ?";
+
+	        if($stmt = mysqli_prepare($link, $sql)){
+	            // Bind variables to the prepared statement as parameters
+	            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+	            // Set parameters
+	            $param_username = $username;
+
+	            // Attempt to execute the prepared statement
+	            if(mysqli_stmt_execute($stmt)){
+	                // Store result
+	                mysqli_stmt_store_result($stmt);
+
+	                // Check if username exists, if yes then verify password
+	                if(mysqli_stmt_num_rows($stmt) == 1){
+	                    // Bind result variables
+	                    mysqli_stmt_bind_result($stmt, $username, $hashed_password, $userType);
+
+	                    if(mysqli_stmt_fetch($stmt)){
+	                        if(password_verify($password, $hashed_password)){
+	                            /* Password is correct, so start a new session and
+	                            save the username to the session */
+	                            session_start();
+	                            $_SESSION['username'] = $username;
+
+															// Check if user is admin
+															if(strcmp($userType,"Admin")==0){
+																header("location: index.php");
+																exit();
+															}else{
+																header("location: usersetting-user.php");
+																exit();
+															}
+
+	                        } else{
+	                            // Display an error message if password is not valid
+	                            $password_err = 'Incorrect Password';
+	                        }
+	                    }
+	                } else{
+	                    // Display an error message if username doesn't exist
+	                    $username_err = 'No account found with that username.';
+	                }
+	            } else{
+	                echo "Oops! Something went wrong. Please try again later.";
+	            }
+	        }
+
+	        // Close statement
+	        mysqli_stmt_close($stmt);
+	    }
+
+	    // Close connection
+	    mysqli_close($link);
+	}
+	?>
+
 <!DOCTYPE html>
 <head>
 	<title>WebDTS</title>
@@ -14,34 +98,33 @@
 	<div class="wrapper">
 			<table class="layout-table">
 				<tr>
-				<td id="side-left" style="width: 251px" valign="top" >
-					<div  id="login">
-						<h3 class="tb-title">Login</h3>
-							<form action="" method="post">
-								<div class="field-up">
-										<label class="login-info">Username</label>
-										<input type="text" name="username" id="login-user" />
-								</div>
-								<div class="field-up">
-										<label class="login-info">Password</label>
-										<input type="password" name="pass" id="login-pass"/>
-								</div>
-								<input type="submit" name="btnLogin" id="btnLogin" value="Login" />
-								<p id="forgotPass"><a href="#">Forgot Password?</a></p>
-							</form>
-
-					</div></td>
 				<td class="banner" style="width: 510px" valign="top" >
 						<center><img src="img/banner-dts.jpg" alt="banner" id="banner-dts"></center>
 						<h2>MinDA Web-Based Document Tracking System</h2>
-						<p>Welcome to the new Web-Based Document Tracking System of Mindanao Development Authority. Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. </p>
+						<p>Welcome to the new Web-Based Document Tracking System of Mindanao Development Authority. This can make an electronic copy of the incoming documents in MinDA which can be easily monitor the status of the said documents. Through using a document tracking system, MinDA now has the ease of access in their documents. </p>
 				</td>
 
-				<td class="events" style="width: 251px" valign="top">
-					<h3 class="tb-title">Upcoming Events</h3>
-				</td>
-			</tr>
+				<td class="login-td" style="width: 251px" valign="top" >
+					<div  class="login">
+						<h3 class="tb-title">Login</h3>
+							<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+								<div class="field-up <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
 
+										<center><input type="text" name="username" class="input-txt" value="<?php echo $username; ?>" placeholder="Username"/><br>
+										<span><?php echo $username_err; ?></span></center>
+								</div>
+								<div class="field-up <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+										<center><input type="password" name="password" class="input-txt" placeholder="Password"/><br>
+										<span><?php echo $password_err; ?></span></center>
+								</div>
+
+								<center><input type="submit" name="btnLogin" class="btnLogin" value="Login" /></center>
+							</form>
+
+					</div></td>
+
+
+				</tr>
 			</table>
 	</div>
 
