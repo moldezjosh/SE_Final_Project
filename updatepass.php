b98571117da49c771e1af5b83f76264c6a096752
Joshua Mark Moldez <j.markmoldez@gmail.com>
2017-11-27 10:13:03 +0800

    modified updatepass file

--- updatepass.php
+++ updatepass.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+
+require_once 'include/update-pass.php';
+?>
+
 <!DOCTYPE html>
 <head>
 	<title>WebDTS</title>
@@ -25,7 +31,7 @@
       </form>
 
       <div class="dropdown">
-        <button onclick="dropFunc()" class="dropbtn">Admin</button>
+        <button onclick="dropFunc()" class="dropbtn"><?php echo $_SESSION['username']; ?></button>
         <div id="myDropdown" class="dropdown-content">
 					<a href="index.php">Dashboard</a>
           <a href="usersetting.php">My Menus</a>
@@ -49,29 +55,33 @@
           <td class="am-display">
             <h3 class="nav-header">update password</h3>
               <div class="menu-display" style="height: 350px">
-								<form>
+								<form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
 									<table class="update-info">
 										<tr height="80" class="curr-pass">
 												<td class="update-td">Current Password</td>
 												<td class="update-textbox">
-														<input type="text" name="username"/>
+														<input type="password" name="password" value=""/><br>
+														<span class="help-block"><?php echo $password_err;?></span>
 												</td>
 										</tr>
 
 										<tr height="80">
 											<td class="update-td">New Password</td>
 											<td class="update-textbox">
-													<input type="text" name="email"/>
+													<input type="password" name="new_password" value=""/><br>
+													<span class="help-block"><?php echo $new_password_err;?></span>
 											</td>
 										</tr>
 									<tr height="80" valign="top">
 											<td class="update-td">Repeat New Password</td>
 											<td class="update-textbox">
-													<input type="text" name="fullname"/>
+													<input type="password" name="confirm_password" value=""/><br>
+													<span class="help-block"><?php echo $confirm_password_err;?></span>
 											</td>
 										</tr>
 
 									</table>
+									<input type="hidden" name="id" value="<?php echo $id; ?>"/>
 									<center>
 									<div class="update-in-pass">
 									<input type="submit" name="btnSave" class="btnOk" value="Update" />
